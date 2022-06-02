23e27c9fcb94dd4ab517d07be798d9a3cc7bb02d
dima forostenko <forostenkochampion@gmail.com>
2022-06-02 23:29:52 +0300

    finalize class Group

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-class User{
-  constructor(name,surname){
- this.name = name;
- this.surname = surname;
+class User {
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+  }
+  InformAbout() {
+    return `$this.name $this.surname`;
+  }
 }
-InformAbout(){
-  return `$this.name $this.surname`
-}
-}
-class Stydent extends User{
-constructor(year){
-  this.start=year
-  this.data=2022
-}
-  get Course(){
-    if() {
-     return this.data  - this.start
-    }else{
-      this.Course > 5
-      return console.log('not rules date')
+class Stydent extends User {
+  InformAbout() {
+    super.InformAbout;
+  }
+  constructor(year) {
+    this.start = year;
+    this._data = 2022;
+  }
+  get Course() {
+    return this.data - this.start;
+  }
+  set Course(newYear) {
+    this.start = newYear;
+    if (this.Course) {
+      this.Course > 5;
+      throw TypeError;
     }
   }
-  
+}
+
+const nameGroup = [
+  { name: "Андрей" ,surname: "Иванов"},
+  { name: "Кристина",surname:"Голкина"},
+];
+class Group {
+  constructor(Stydent, nameGroup) {
+    this.nameGroups = nameGroup;
+    this.Stydent = Stydent;
+  }
+
+  showStudents() {
+    return `$this.name $this.surname`
+  }
 }

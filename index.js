9b187e9b95ef373791fcde7ad0368417d0c9e559
dima forostenko <forostenkochampion@gmail.com>
2022-06-08 16:24:38 +0300

    'add start version'

--- index.js
+++ index.js
@@ -1,43 +1,51 @@
+"use string";
+
 class User {
   constructor(name, surname) {
     this.name = name;
     this.surname = surname;
   }
-  InformAbout() {
-    return `$this.name $this.surname`;
-  }
-}
-class Stydent extends User {
-  InformAbout() {
-    super.InformAbout;
-  }
-  constructor(year) {
-    this.start = year;
-    this._data = 2022;
-  }
-  get Course() {
-    return this.data - this.start;
-  }
-  set Course(newYear) {
-    this.start = newYear;
-    if (this.Course) {
-      this.Course > 5;
-      throw TypeError;
-    }
-  }
-}
+    getInformAbout() {
+       return `${this.name} ${this.surname}`;
+     }
+  };
+  const PeopleB = new User('andy','ferguson');
+console.log(PeopleB.getInformAbout());
+// class Stydent extends User {
+//   InformAbout() {
+//     super.InformAbout;
+    
+//   }
+//   constructor(year) {
+//     this.start = year;
+//     this._data = 2022;
+//   }
+//   get Course() {
+//     return this._data - this.start;
+//   }
+//   set Course(newYear) {
+//     this.start = newYear;
+//     if (this.Course) {
+//       this.Course > 5;
+//       throw TypeError;
+//     }
+//   }
+// };
 
-const nameGroup = [
-  { name: "Андрей" ,surname: "Иванов"},
-  { name: "Кристина",surname:"Голкина"},
-];
-class Group {
-  constructor(Stydent, nameGroup) {
-    this.nameGroups = nameGroup;
-    this.Stydent = Stydent;
-  }
+// const nameGroup=[
+//   { name: 'Андрей' ,surname: "Иванов"},
+//   {name: "Кристина",surname:"Голкина"},
+// ]
+// class Group {
+//   constructor(Stydent, nameGroup) {
+//     this.nameGroups = nameGroup;
+//     this.Stydent = Stydent;
+//   };
+//   showStudents() {
+//     Function (...nameGroup)
+//     return `$this.name $this.surname`
+    
+//   }
 
-  showStudents() {
-    return `$this.name $this.surname`
-  }
-}
+  
+// }
